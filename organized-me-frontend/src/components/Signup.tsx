f442bb673aefb71d069ae0a0eee1d1cff38a315c
eavlongs <eavlongs@gmail.com>
2023-10-19 18:35:31 +0700

    removed sass from project, and started using shadcn for design, finished the sign up and log in page

--- organized-me-frontend/src/components/Signup.tsx
+++ organized-me-frontend/src/components/Signup.tsx
@@ -1,18 +1,51 @@
 import { supabase } from "../lib/supabase";
-import { FormEvent, useEffect, useState } from "react";
-import "../styles/signin-signup.scss";
-import {
-    SignupErrorType,
-    comparePassword,
-    validateEmail,
-    validateName,
-    validatePassword,
-} from "../helper";
+import { useState } from "react";
+import "@/globals.css";
+
+import * as z from "zod";
 import {
     GithubLoginButton,
     GoogleLoginButton,
 } from "react-social-login-buttons";
 import { useNavigate } from "react-router-dom";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "./ui/button";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import redCloseIcon from "@/assets/close-red.png";
+
+const signUpSchema = z
+    .object({
+        firstName: z
+            .string()
+            .trim()
+            .min(2, { message: "Must contain at least 2 characters" }),
+        lastName: z
+            .string()
+            .trim()
+            .min(2, { message: "Must contain at least 2 characters" }),
+        email: z
+            .string()
+            .min(1, { message: "This field has to be filled." })
+            .email("Invalid email"),
+        password: z
+            .string()
+            .min(8, { message: "Password must contain at least 8 characters" }),
+        confirmPassword: z.string(),
+    })
+    .refine((data) => data.password === data.confirmPassword, {
+        path: ["confirmPassword"],
+        message: "Password and confirm password don't match",
+    });
 
 async function loginGoogle() {
     const { error } = await supabase.auth.signInWithOAuth({
@@ -31,221 +64,187 @@ async function loginGithub() {
 }
 
 function Signup() {
-    const [firstName, setFirstName] = useState("");
-    const [lastName, setLastName] = useState("");
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [confirmPassword, setConfirmPassword] = useState("");
-    const [loading, setLoading] = useState(false);
-    const [errors, setErrors] = useState<SignupErrorType>({
-        firstName: false,
-        lastName: false,
-        email: false,
-        password: false,
-        confirmPassword: false,
+    const form = useForm<z.infer<typeof signUpSchema>>({
+        resolver: zodResolver(signUpSchema),
+        mode: "onChange",
+        defaultValues: {
+            firstName: "",
+            lastName: "",
+            email: "",
+            password: "",
+            confirmPassword: "",
+        },
     });
+    const [loading, setLoading] = useState(false);
+    const [supabaseError, setSupabaseError] = useState("");
     const navigate = useNavigate();
 
-    useEffect(() => {
-        if (!comparePassword(password, confirmPassword)) {
-            setErrors((prev) => ({
-                ...prev,
-                confirmPassword: true,
-            }));
-        } else
-            setErrors((prev) => ({
-                ...prev,
-                confirmPassword: false,
-            }));
-    }, [confirmPassword]);
-
-    useEffect(() => {
-        if (!validatePassword(password)) {
-            setErrors((prev) => ({ ...prev, password: true }));
-        } else setErrors((prev) => ({ ...prev, password: false }));
-    }, [password]);
-
-    useEffect(() => {
-        if (!validateEmail(email)) {
-            setErrors((prev) => ({ ...prev, email: true }));
-        } else setErrors((prev) => ({ ...prev, email: false }));
-    }, [email]);
-
-    async function submit(e: FormEvent) {
-        e.preventDefault();
-
-        // validation stage for name
-        if (!validateName(firstName))
-            setErrors((prev) => ({ ...prev, firstName: true }));
-        else if (!validateName(firstName))
-            setErrors((prev) => ({ ...prev, firstName: false }));
-
-        if (!validateName(lastName))
-            setErrors((prev) => ({ ...prev, lastName: true }));
-        else if (!validateName(lastName))
-            setErrors((prev) => ({ ...prev, lastName: false }));
-
-        let errorRemains = false;
-        for (const key in errors) {
-            if (
-                errors.hasOwnProperty(key) &&
-                // @ts-ignore
-                (errors[key] as boolean) === true
-            ) {
-                errorRemains = true;
-                // @ts-ignore
-                console.log(key, errors[key]);
-                break;
-            }
-        }
-
-        if (errorRemains) return;
+    async function submit(data: z.infer<typeof signUpSchema>) {
         setLoading(true);
+        setSupabaseError("");
 
         const { error } = await supabase.auth.signUp({
-            email,
-            password,
+            email: data.email,
+            password: data.password,
             options: {
                 data: {
-                    first_name: firstName,
-                    last_name: lastName,
+                    first_name: data.firstName,
+                    last_name: data.lastName,
                 },
             },
         });
 
-        if (error !== null)
-            setErrors((prev) => ({ ...prev, supabaseError: error }));
+        if (error !== null) setSupabaseError(error.message);
         else navigate("/");
         setLoading(false);
     }
 
     return (
-        <form onSubmit={submit} method='POST'>
-            <h1 className='title'>Sign Up</h1>
-            <div className='two-input'>
-                <label htmlFor='first-name'>First Name</label>
-                <label htmlFor='last-name'>Last Name</label>
-                <input
-                    type='text'
-                    name='first-name'
-                    id='first-name'
-                    onChange={(e) => {
-                        setFirstName(e.target.value);
-                    }}
-                    required
-                />
-                <input
-                    type='text'
-                    name='last-name'
-                    id='last-name'
-                    onChange={(e) => {
-                        setLastName(e.target.value);
-                    }}
-                    required
-                />
-                <span className='validation errors'>
-                    {errors?.firstName
-                        ? "Must contain at least 2 characters"
-                        : null}
-                </span>
+        <>
+            <div className='flex items-center justify-center h-screen'>
+                <Form {...form}>
+                    <form
+                        onSubmit={form.handleSubmit(submit)}
+                        method='POST'
+                        className='w-1/2 max-w-[650px] border-2 mx-auto p-6 flex flex-col gap-y-3'
+                    >
+                        <p className='scroll-m-20 text-xl font-semibold tracking-tight text-center'>
+                            Create an Account
+                        </p>
 
-                <span className='validation errors'>
-                    {errors?.lastName
-                        ? "Must contain at least 2 characters"
-                        : null}
-                </span>
+                        <FormField
+                            control={form.control}
+                            name='firstName'
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>First Name</FormLabel>
+                                    <FormControl>
+                                        <Input
+                                            {...field}
+                                            autoComplete='given-name'
+                                        />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                        <FormField
+                            control={form.control}
+                            name='lastName'
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Last Name</FormLabel>
+                                    <FormControl>
+                                        <Input
+                                            {...field}
+                                            autoComplete='family-name'
+                                        />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                        <FormField
+                            control={form.control}
+                            name='email'
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Email</FormLabel>
+                                    <FormControl>
+                                        <Input
+                                            {...field}
+                                            type='email'
+                                            autoComplete='username'
+                                        />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                        <FormField
+                            control={form.control}
+                            name='password'
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Password</FormLabel>
+                                    <FormControl>
+                                        <Input
+                                            {...field}
+                                            type='password'
+                                            autoComplete='new-password'
+                                        />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                        <FormField
+                            control={form.control}
+                            name='confirmPassword'
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Confirm Password</FormLabel>
+                                    <FormControl>
+                                        <Input
+                                            {...field}
+                                            type='password'
+                                            autoComplete='new-password'
+                                        />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                        <Button
+                            type='submit'
+                            disabled={loading}
+                            className='mt-4'
+                        >
+                            {loading ? "Signing Up" : "Sign Up"}
+                        </Button>
+                        {/* or */}
+                        <div className='flex items-center'>
+                            <div className='flex-grow bg bg-gray-300 h-0.5'></div>
+                            <div className='flex-grow-0 mx-5 leading-7 '>
+                                or
+                            </div>
+                            <div className='flex-grow bg bg-gray-300 h-0.5'></div>
+                        </div>
+                        <GoogleLoginButton
+                            onClick={loginGoogle}
+                            text='Continue With Google'
+                            align='center'
+                            disabled={loading}
+                        />
+                        <GithubLoginButton
+                            onClick={loginGithub}
+                            text='Continue With Github'
+                            align='center'
+                            disabled={loading}
+                        />
+                    </form>
+                </Form>
             </div>
-            <div className='one-input'>
-                <label htmlFor='email'>Email</label>
-                <input
-                    type='email'
-                    name='email'
-                    id='email'
-                    onChange={(e) => {
-                        setEmail(e.target.value);
-                    }}
-                    required
-                />
-                {email.length !== 0 ? (
-                    <span
-                        className={
-                            "validation" +
-                            (!errors?.email ? " success" : " errors")
-                        }
-                    >
-                        Email must be a valid email
-                    </span>
-                ) : null}
-            </div>
-            <div className='one-input'>
-                <label htmlFor='password'>Password</label>
-                <input
-                    type='password'
-                    name='password'
-                    id='password'
-                    onChange={(e) => {
-                        setPassword(e.target.value);
-                    }}
-                    required
-                />
-                {password.length !== 0 ? (
-                    <span
-                        className={
-                            "validation" +
-                            (errors?.password ? " error" : " success")
-                        }
-                    >
-                        Password must be longer than 8 characters
-                    </span>
-                ) : null}
-            </div>
-            <div className='one-input'>
-                <label htmlFor='confirm-password'>Confirm Password</label>
-                <input
-                    type='password'
-                    name='confirm-password'
-                    id='confirm-password'
-                    onChange={(e) => {
-                        setConfirmPassword(e.target.value);
-                    }}
-                    required
-                />
-                {confirmPassword.length !== 0 ? (
-                    <span
-                        className={
-                            "validation" +
-                            (errors?.confirmPassword ? " error" : " success")
-                        }
-                    >
-                        Password and confirm password must match
-                    </span>
-                ) : null}
-            </div>
-            <button
-                type='submit'
-                className={"signup-button" + (loading ? "" : " hover-darker")}
-                disabled={loading}
-            >
-                {loading ? "Signing Up" : "Sign Up"}
-            </button>
-            {errors?.supabaseError ? (
-                <span className='validation error supabase-error'>
-                    {errors.supabaseError.message}
-                </span>
+            {supabaseError.length !== 0 ? (
+                <Alert
+                    variant='destructive'
+                    className='absolute top-1 right-1 w-72'
+                >
+                    <div className='flex justify-between'>
+                        <AlertTitle>An Error Occured</AlertTitle>
+                        <img
+                            src={redCloseIcon}
+                            alt='close icon'
+                            className='w-4 h-4 cursor-pointer'
+                            onClick={() => {
+                                setSupabaseError("");
+                            }}
+                        />
+                    </div>
+                    <AlertDescription>{supabaseError}</AlertDescription>
+                </Alert>
             ) : null}
-
-            <span className='separator'>or</span>
-            <div className='providers'>
-                <GoogleLoginButton
-                    onClick={loginGoogle}
-                    text='Continue With Google'
-                />
-                <GithubLoginButton
-                    onClick={loginGithub}
-                    text='Continue With Github'
-                />
-            </div>
-        </form>
+        </>
     );
 }
 
